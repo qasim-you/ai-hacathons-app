bf00c8fe86f66ed6caa98e774f68bf3511ad2f79
Muhammad Qasim <123858146+qasim-you@users.noreply.github.com>
2025-01-29 22:54:17 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gradient-to-b from-blue-100 via-white to-indigo-100">
+    <main className="min-h-screen  from-blue-100 via-white to-indigo-100">
       <Hero />
       <Dashboard />
     </main>
